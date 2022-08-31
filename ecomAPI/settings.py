0c319871fb7259e7f7f5885e76d8da5de3394f4c
Chymdy Ugah <ugahchymdy@gmail.com>
2022-08-31 11:17:58 +0100

    'fix'

--- ecomAPI/settings.py
+++ ecomAPI/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_x^_0qqmw0%si$c%$+6t&$%u)7@$o_7qv(oio$_(qo)@k(g#c!'
+# SECRET_KEY = 'django-insecure-_x^_0qqmw0%si$c%$+6t&$%u)7@$o_7qv(oio$_(qo)@k(g#c!'
+SECRET_KEY = 'YOUR KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
